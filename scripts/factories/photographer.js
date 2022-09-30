6c5e5d92f9bf89674b215b1b498983f9a662689a
Thimothée VANIER <tva@subteno.com>
2022-09-30 16:20:27 +0200

    photographers displayed from data json

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,21 @@
-export function photographerFactory(data) {
-    const { name, portrait } = data;
-    const picture = `assets/photographers/${portrait}`;
-    function getUserCardDOM() {
+export class photographerFactory {
+    name;
+    portrait;
+    pictureURL;
+    constructor(data) {
+        this.name = data.name;
+        this.portrait = data.portrait;
+        this.pictureURL = `/assets/photos/Photographers ID Photos/${this.portrait}`;
+    }
+    getUserCardDOM() {
         const article = document.createElement('article');
         const img = document.createElement('img');
-        img.setAttribute("src", picture);
+        img.setAttribute("src", this.pictureURL);
         const h2 = document.createElement('h2');
-        h2.textContent = name;
+        h2.textContent = this.name;
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
     }
-    return { name, picture, getUserCardDOM };
 }
 //# sourceMappingURL=photographer.js.map
