a32979216f3736371f651828ad52aefb29d4977d
Thimothée VANIER <tva@subteno.com>
2022-09-30 16:05:56 +0200

    init ts

---
+++ @types/index.ts
@@ -0,0 +1,11 @@
+export type photographer = {
+  name: string,
+  id: number,
+  city: string,
+  country: string,
+  tagline: string,
+  price: number,
+  portrait: string,
+}
+
+export type photographerList = photographer[];
